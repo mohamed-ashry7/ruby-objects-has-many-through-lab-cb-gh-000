6936029f2137e13d47e45f582f16a6aef53cbe61
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:50:12 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,7 +10,7 @@ class Doctor
     @@all
   end
   def new_appointment(patient , date)
-    Appointment.new (date,patient,self )
+    Appointment.new(date,patient , self)
   end
 
   def appointments
