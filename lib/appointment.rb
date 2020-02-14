1549de4ab69283c4b6902ef4ba829dd506e52fbc
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:47:59 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment
+  attr_accessor :doctor , :patient , :date
+  @@all = [] 
+  def initialize(date , patient , doctor)
+    @doctor = doctor 
+    @patient = patient 
+    @date = date 
+    @@all << self 
+  end 
