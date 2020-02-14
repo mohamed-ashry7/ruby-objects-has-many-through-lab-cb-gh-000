3268704b618a6ed79c0f0e95581999cd10bbacd9
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:35:29 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,16 +1,16 @@
 class Genre
 
-  attr_accessor :genre 
-  
+  attr_accessor :genre
+
   @@all = []
   def initialize(genre)
     @genre = genre
   end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
-  def songs 
+  def songs
     
 end
