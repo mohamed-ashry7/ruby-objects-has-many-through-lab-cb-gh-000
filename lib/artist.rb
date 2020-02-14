048648ef412ec81f582bfd58d26ff6ae4a87bfbb
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:31:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ class Artist
   @@all = []
   def initialize (name )
     @name = name
-    @@all <<self 
+    @@all <<self
   end
   def new_song(name,genre)
     song = Song.new(name, self , genre)
