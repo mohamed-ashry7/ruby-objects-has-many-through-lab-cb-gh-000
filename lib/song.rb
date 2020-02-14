315ad35a8674f2765983299fea5d09127df674c2
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:37:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,10 @@
 
 def Song
 
-  attr_accessor :artist , :genre 
+  attr_accessor :artist , :genre , :name 
+
+  def initialize(name , artist , genre ) 
+    @artist = artist 
+    @name = name 
+    @genre = genre 
+    @@all << self 
